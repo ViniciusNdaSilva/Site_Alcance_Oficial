13485a3bccedafcb6d4b6810d1ae96a2b863711b
Elder Nogueira <elder.renan13@gmail.com>
2023-10-10 10:55:53 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-$(document).ready(function(){let e=$(".nav-item"),a=$("#mainSlider"),n=$("#pastores"),t=$("#Palavra-semana"),i=$("#Redes-Sociais"),o=$("#Celula"),l=$("#Contribuicoes-menu"),s="";$(e).click(function(){let e=$(this).attr("id");s="pastores-menu"==e?n:"Palavra-menu"==e?t:"Redes-Sociais-menu"==e?i:"Celula-menu"==e?o:"Contri-menu"==e?l:a,$([document.documentElement,document.body]).animate({scrollTop:$(s).offset().top-70},1500)})});
+$(document).ready(function(){
+let e=$(".nav-item")
+,a=$("#mainSlider")
+,n=$("#pastores")
+,t=$("#Palavra-semana")
+,i=$("#Redes-Sociais")
+,o=$("#Celula")
+,l=$("#contribuicoes")
+,m=$("#missoes")
+,s=""
+;
+$(e).click(function(){
+let e=$(this).attr("id")
+;s="pastores-menu"==e?n:"Palavra-menu"==e?t:"Redes-Sociais-menu"==e?i:"Celula-menu"==e?o:"Contri-menu"==e?l:a:"Missao-menu"==e?m
+,$([document.documentElement,document.body]).animate({scrollTop:$(s).offset().top-70},1500)})
+});
